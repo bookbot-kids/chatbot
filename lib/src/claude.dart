ed63c6654de5eae1ae731f2526d0495796d39a3e
duc <duc@appiphany.com.au>
2024-10-03 18:13:50 +0700

    Replace claude sdk

--- lib/src/claude.dart
+++ lib/src/claude.dart
@@ -1,29 +1,34 @@
-import 'package:anthropic_sdk_dart/anthropic_sdk_dart.dart' as sdk;
+import 'package:anthropic_sdk_dart/anthropic_sdk_dart.dart';
 import 'package:chatbot/src/llm.dart';
 import 'package:chatbot/src/prompt_config.dart';
-import 'package:anthropic_dart/anthropic_dart.dart';
 
 /// Claude service
 class Claude extends LLM {
-  late AnthropicService service;
-  late sdk.AnthropicClient serviceSdk;
+  late AnthropicClient serviceSdk;
 
   Claude({required super.key, super.serviceConfig}) {
-    service = AnthropicService(key);
-    serviceSdk = sdk.AnthropicClient(apiKey: key);
+    serviceSdk = AnthropicClient(apiKey: key);
   }
 
   @override
   Future<Iterable<String>> generateText(
       LLMMessage message, PromptConfig config) async {
-    final request = Request(
-        model: config.engine,
-        maxTokens: config.maxTokens,
-        messages: [Message(role: message.role, content: message.message)]);
-    final response = await service.sendRequest(
-        request: request, debug: serviceConfig.enableLog);
-    final content = response.content ?? [];
-    return content.map((e) => e.text ?? '');
+    final MessageRole role;
+    if (message.role == 'assistant') {
+      role = MessageRole.assistant;
+    } else {
+      role = MessageRole.user;
+    }
+
+    final request = CreateMessageRequest(
+        model: Model.modelId(config.engine),
+        maxTokens: config.maxTokens ?? 4096,
+        messages: [
+          Message(content: MessageContent.text(message.message), role: role)
+        ]);
+
+    final response = await serviceSdk.createMessage(request: request);
+    return [response.content.text];
   }
 
   @override
@@ -34,49 +39,55 @@ class Claude extends LLM {
   @override
   Future<Iterable<String>> generateConversation(
       List<LLMMessage> messages, PromptConfig config) async {
-    final conversations =
-        messages.map((e) => Message(content: e.message, role: e.role)).toList();
+    final conversations = messages.map((e) {
+      final MessageRole role;
+      if (e.role == 'assistant') {
+        role = MessageRole.assistant;
+      } else {
+        role = MessageRole.user;
+      }
 
-    final request = Request(
-        model: config.engine,
-        maxTokens: config.maxTokens,
+      return Message(content: MessageContent.text(e.message), role: role);
+    }).toList();
+
+    final request = CreateMessageRequest(
+        model: Model.modelId(config.engine),
+        maxTokens: config.maxTokens ?? 4096,
         messages: conversations);
-    final response = await service.sendRequest(
-        request: request, debug: serviceConfig.enableLog);
-    final content = response.content ?? [];
-    return content.map((e) => e.text ?? '');
+
+    final response = await serviceSdk.createMessage(request: request);
+    return [response.content.text];
   }
 
   @override
   Stream<Iterable<String>> generateStream(
       List<LLMMessage> messages, PromptConfig config) {
     final conversations = messages.map((e) {
-      final sdk.MessageRole role;
+      final MessageRole role;
       if (e.role == 'assistant') {
-        role = sdk.MessageRole.assistant;
+        role = MessageRole.assistant;
       } else {
-        role = sdk.MessageRole.user;
+        role = MessageRole.user;
       }
 
-      return sdk.Message(
-          content: sdk.MessageContent.text(e.message), role: role);
+      return Message(content: MessageContent.text(e.message), role: role);
     }).toList();
-    final request = sdk.CreateMessageRequest(
-        model: sdk.Model.modelId(config.engine),
+    final request = CreateMessageRequest(
+        model: Model.modelId(config.engine),
         maxTokens: config.maxTokens ?? 4096,
         messages: conversations);
     final stream = serviceSdk.createMessageStream(request: request);
     return stream.map((data) {
       final result = data.map(
-            messageStart: (sdk.MessageStartEvent v) {},
-            messageDelta: (sdk.MessageDeltaEvent v) {},
-            messageStop: (sdk.MessageStopEvent v) {},
-            contentBlockStart: (sdk.ContentBlockStartEvent v) {},
-            contentBlockDelta: (sdk.ContentBlockDeltaEvent v) {
+            messageStart: (MessageStartEvent v) {},
+            messageDelta: (MessageDeltaEvent v) {},
+            messageStop: (MessageStopEvent v) {},
+            contentBlockStart: (ContentBlockStartEvent v) {},
+            contentBlockDelta: (ContentBlockDeltaEvent v) {
               return v.delta.text;
             },
-            contentBlockStop: (sdk.ContentBlockStopEvent v) {},
-            ping: (sdk.PingEvent v) {},
+            contentBlockStop: (ContentBlockStopEvent v) {},
+            ping: (PingEvent v) {},
           ) ??
           '';
       return [result];
